1b8639c0e87207799a0f4464a504c9f899a1b550
Ascen Keeprov <assenkiprov@yahoo.com>
2017-09-26 16:33:28 +0300

    Secondary update of local VS clone

--- RepoPrime/Program.cs
+++ RepoPrime/Program.cs
@@ -8,7 +8,8 @@ namespace Program
 		{
 			Console.Write("Please enter your name: ");
 			string name = Console.ReadLine().Trim();
-			Console.WriteLine($"Welcome to GitHub, {name}");
+			Console.WriteLine($"░░▒▒▓▓ Welcome to GitHub, {name}! ▓▓▒▒░░");
+			//Secondary update of local VS clone
 		}
 	}
 }
