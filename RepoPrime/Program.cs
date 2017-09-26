5cd5a5d9f314a7d0bf8d8148e83462e14198a4c1
Ascen Keeprov <assenkiprov@yahoo.com>
2017-09-26 16:07:29 +0300

    Added default .gitattributes and .gitignore

---
+++ RepoPrime/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program
+{
+	class Startup
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Please enter your name: ");
+			string name = Console.ReadLine().Trim();
+			Console.WriteLine($"Welcome to GitHub, {name}");
+		}
+	}
+}
