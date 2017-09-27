1308c7d3cd85aa91165cafb01b9acd311ae57a18
Ascen Keeprov <assenkiprov@yahoo.com>
2017-09-27 09:10:24 +0300

    Conflict sim redux - TortoiseGit

--- RepoPrime/Program.cs
+++ RepoPrime/Program.cs
@@ -12,6 +12,5 @@ namespace Program
 		}
 	}
 }
-/*Secondary conflict simulation - GitBash.
- *Secondary conflict simulation - Visual Studio.
- * Primary failed due to omitted PUSH*/
+/*Secondary conflict simulation - TortoiseGit.
+  * Primary failed due to omitted PUSH*/
